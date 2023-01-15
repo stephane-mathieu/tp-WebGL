92a8939ae9ade9668edc90d4d83ff97bd53a1243
Stéphane-Mathieu <stephane@mastercv.fr>
2023-01-15 15:18:19 +0100

    variable

--- js/app2.js
+++ js/app2.js
@@ -24,9 +24,18 @@ var _elapsedTime = 0 //  Durée écoulée entre la frame actuelle N et N-1
 var _hydrogene // Déclaration de la variable hydrogene
 var electronMesh2 // Déclaration de la variable hydrogene
 var electronMesh // Déclaration de la variable hydrogene
+var Terre // Déclaration de la variable hydrogene
+var Mars // Déclaration de la variable hydrogene
+var Lune // Déclaration de la variable hydrogene
+var Saturne // Déclaration de la variable hydrogene
+var Soleil // Déclaration de la variable hydrogene
 
-var _focusEnabled = false // = true si le focus de l'atome est activé
-var _focusEnabled02 = false
+
+var _focusTerre
+var _focusLune
+var _focusMars
+var _focusSoleil
+var _focusSaturn
 var _background
 
 var DatGUISettings = { // Parametres disponible dans le menu dat GUI
@@ -86,10 +95,7 @@ function InitScene() {
     // Création et positionnement de l'atome d'hygdrogene
     _hydrogene = new Hydrogene(5, 25, 5)
     _hydrogene.obj.position.x = 0
-        // console.log(_hydrogene);
-    console.log(_hydrogene);
-    console.log(electronMesh2);
-    console.log(electronMesh);
+
     const size = 100;
     const divisions = 10;
 
@@ -116,11 +122,11 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const proton = new THREE.TextureLoader().load('/assets/textures/sun-texture.jpg');
 
     const protonMat = new THREE.MeshLambertMaterial({ map: proton })
-    const protonMesh = new THREE.Mesh(protonGeo, protonMat)
+    Soleil = new THREE.Mesh(protonGeo, protonMat)
         // Ajoute le proton à l'objet
-    obj.add(protonMesh)
+    obj.add(Soleil)
         // Place le proton au centre de l'objet
-    protonMesh.position.set(0, 0, 0)
+    Soleil.position.set(0, 0, 0)
 
     // Création de l'orbite, représentée par un anneau, rayon = protonRadius * 3, epaisseur = 0.05
     const orbitGeo = new THREE.TorusGeometry(75, 0.05, 32, 64)
@@ -141,12 +147,12 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const _terre = new THREE.TextureLoader().load('/assets/textures/earth-texture.jpg');
 
     const electronMat = new THREE.MeshLambertMaterial({ map: _terre })
-    electronMesh = new THREE.Mesh(electronGeo, electronMat)
-    obj.add(electronMesh)
+    Terre = new THREE.Mesh(electronGeo, electronMat)
+    obj.add(Terre)
         // Ajout l'electron à l'orbite afin de le déplacer selon les mouvements de l'orbite 
-    orbitMesh.add(electronMesh)
+    orbitMesh.add(Terre)
         // Placement de l'electron sur l'orbit
-    electronMesh.position.y = 75
+    Terre.position.y = 75
 
     // Création de l'orbite lune
     const orbitGeo3 = new THREE.TorusGeometry(10, 0.05, 32, 64)
@@ -155,7 +161,7 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     obj.add(orbitMesh3)
         // Place l'anneau au centre de l'objet
     orbitMesh3.position.set(0, 0, 0)
-    electronMesh.add(orbitMesh3)
+    Terre.add(orbitMesh3)
     orbitMesh3.rotation.x = THREE.MathUtils.degToRad(25)
         // Creation d'un axis helper pour visualiser les axes de l'orbite
     const axesHelper3 = new THREE.AxesHelper(5)
@@ -168,13 +174,13 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const _lune = new THREE.TextureLoader().load('/assets/textures/moon-texture.jpg');
 
     const electronMat4 = new THREE.MeshLambertMaterial({ map: _lune })
-    const electronMesh4 = new THREE.Mesh(electronGeo4, electronMat4)
-    obj.add(electronMesh4)
+    Lune = new THREE.Mesh(electronGeo4, electronMat4)
+    obj.add(Lune)
         // Ajout l'electron à l'orbite afin de le déplacer selon les mouvements de l'orbite 
-    orbitMesh3.add(electronMesh4)
+    orbitMesh3.add(Lune)
 
     // Placement de l'electron sur l'orbit
-    electronMesh4.position.y = 10
+    Lune.position.y = 10
 
     // Création de l'orbite01
     const orbitGeo1 = new THREE.TorusGeometry(100, 0.05, 32, 64)
@@ -195,12 +201,12 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const _mars = new THREE.TextureLoader().load('/assets/textures/mars-texture.jpg');
 
     const electronMat2 = new THREE.MeshLambertMaterial({ map: _mars })
-    electronMesh2 = new THREE.Mesh(electronGeo2, electronMat2)
-    obj.add(electronMesh2)
+    Mars = new THREE.Mesh(electronGeo2, electronMat2)
+    obj.add(Mars)
         // Ajout l'electron à l'orbite afin de le déplacer selon les mouvements de l'orbite
-    orbitMesh.add(electronMesh2)
+    orbitMesh.add(Mars)
         // Placement de l'electron sur l'orbit
-    electronMesh2.position.y = 100
+    Mars.position.y = 100
 
 
     // Création de l'orbite02
@@ -221,12 +227,12 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const _saturn = new THREE.TextureLoader().load('/assets/textures/saturn-texture.jpg');
 
     const electronMat3 = new THREE.MeshLambertMaterial({ map: _saturn })
-    const electronMesh3 = new THREE.Mesh(electronGeo3, electronMat3)
-    obj.add(electronMesh3)
+    Saturne = new THREE.Mesh(electronGeo3, electronMat3)
+    obj.add(Saturne)
         // Ajout l'electron à l'orbite afin de le déplacer selon les mouvements de l'orbite 
-    orbitMesh.add(electronMesh3)
+    orbitMesh.add(Saturne)
         // Placement de l'electron sur l'orbit
-    electronMesh3.position.y = 160
+    Saturne.position.y = 160
 
     const _saturn_ring = new THREE.TextureLoader().load('/assets/textures/saturn-ring-texture.png');
 
@@ -235,7 +241,7 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
     const mesh = new THREE.Mesh(geometry, material);
     obj.add(mesh)
     _scene.add(mesh);
-    electronMesh3.add(mesh)
+    Saturne.add(mesh)
 
 
 
@@ -244,12 +250,16 @@ function Hydrogene(protonRadius, electronSpeed, speed) {
 
     // On reference nos objets 3D au sein de valeurs liées à l'objet Hydrogene
     this.obj = obj // L'objet 3D dans sa globalité
-    this.proton = protonMesh // Le proton 3D
+    this.proton = Soleil // Le proton 3D
     this.orbit = orbitMesh // l'orbite 3D
     this.electron = electronMesh // l'electron 3D
     this.electronSpeed = electronSpeed // vitesse de rotation de l'orbit (deg/sec)
     this.speed = speed // vitesse de deplacement de l'atome
     this.direction = 1 // sens de direction du deplacement
+    this.planet01 = Terre
+    this.planet02 = Lune
+    this.planet03 = Saturne
+    this.planet04 = Mars
 }
 
 // Fonction qui permet d'initialiser l'UI Dat.GUI
@@ -267,20 +277,60 @@ function OnChangeProtonColor() {
     _hydrogene.proton.material.color.setHex(DatGUISettings.protonColor)
 }
 
-// Fonction appelée lors du clic sur le bouton Focus Atom. Permet à la camera de focus l'atome
-function OnClickFocusAtom() {
-    _focusEnabled = true;
-}
+// // Fonction appelée lors du clic sur le bouton Focus Atom. Permet à la camera de focus l'atome
+// function OnClickFocusTerre() {
+//     _focusTerre = true;
+//     _focusMars = false;
+//     _focusLune = false;
+//     _focusSoleil = false;
+//     _focusSoleil = false;
+// }
 
-function OnClickFocusAtom02() {
-    _focusEnabled02 = true;
+// function OnClickFocusLune() {
+//     _focusLune = true;
+//     _focusTerre = false;
+//     _focusMars = false;
+//     _focusSaturn = false;
+//     _focusSoleil = false;
+// }
+
+// function OnClickFocusSoleil() {
+//     _focusSoleil = true;
+//     _focusLune = false;
+//     _focusTerre = false;
+//     _focusMars = false;
+//     _focusSaturn = false;
+// }
+
+// function OnClickFocusMars() {
+//     _focusMars = true;
+//     _focusLune = false;
+//     _focusTerre = false;
+//     _focusSaturn = false;
+//     _focusSoleil = false;
+// }
+
+// function OnClickFocusSaturn() {
+//     _focusSaturn = true;
+//     _focusLune = false;
+//     _focusTerre = false;
+//     _focusMars = false;
+//     _focusSoleil = false;
+// }
+
+function OnClickFocus(focus) {
+    _focusTerre = focus === "Terre";
+    _focusLune = focus === "Lune";
+    _focusSoleil = focus === "Soleil";
+    _focusMars = focus === "Mars";
+    _focusSaturn = focus === "Saturn";
 }
 // Bind la fonction OnClickFocusAtom à l'event click du bouton
-document.getElementById("Soleil").addEventListener("click", OnClickFocusAtom)
-document.getElementById("Lune").addEventListener("click", OnClickFocusAtom)
-document.getElementById("Mars").addEventListener("click", OnClickFocusAtom)
-document.getElementById("Saturn").addEventListener("click", OnClickFocusAtom)
-document.getElementById("Terre").addEventListener("click", OnClickFocusAtom02)
+document.getElementById("Soleil").addEventListener("click", OnClickFocus("Soleil"))
+document.getElementById("Lune").addEventListener("click", OnClickFocus("Lune"))
+document.getElementById("Mars").addEventListener("click", OnClickFocus("Mars"))
+document.getElementById("Saturn").addEventListener("click", OnClickFocus("Saturn"))
+document.getElementById("Terre").addEventListener("click", OnClickFocus("Terre"))
 
 // Redimensionnement de la fenetre
 function Resize() {
@@ -289,7 +339,7 @@ function Resize() {
     _renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-
+console.log(_focusTerre)
 
 
 // Boucle d'animation
@@ -302,7 +352,7 @@ function Animate() {
     var zAxis = new THREE.Vector3(0, 0, 1)
 
     // // Applique la rotation sur l'axe z. Vitesse = deg/sec * elapsedTime * coef
-    // electronMesh2.rotateOnAxis(zAxis, THREE.MathUtils.degToRad(electronMesh2.electronSpeed * _elapsedTime * DatGUISettings.electronSpeedFactor))
+    // _hydrogene.rotateOnAxis(zAxis, THREE.MathUtils.degToRad(_hydrogene.electronSpeed * _elapsedTime * DatGUISettings.electronSpeedFactor))
 
     // // Change la direction du deplacement selon la position en x
     // if (_hydrogene.obj.position.x > 10) {
@@ -315,27 +365,85 @@ function Animate() {
 
 
     // Si le focus de l'atome est activé
-    if (_focusEnabled == true) {
-        // On définit une position pour le control camera
-        // var offset = new THREE.Vector3(70, 15, 7)
-        // controlPosition = position de l'atome + décalage de 7 unité sur l'axe z
-        var controlPosition = electronMesh.position
-            // Applique la nouvelle position
+    if (_focusTerre == "Terre") {
+
+        // Offset de la caméra par rapport à la planète (offset = rayon planète + 10)
+        var offset = new THREE.Vector3(-20, 20, 60)
+            // Déclare un Vector3 worldPosition
+        var worldPosition = new THREE.Vector3()
+            // Enregistre la world position de la Terre (position globale) dans worldPosition
+        Terre.getWorldPosition(worldPosition)
+            // Applique la position au control
+        var controlPosition = worldPosition.add(offset)
         _control.object.position.copy(controlPosition)
-            //  focus le control sur la position de l'atome
-        _control.target = electronMesh.position
+            // Focus la world position de la Terre
+        _control.target = worldPosition
     }
 
-    if (_focusEnabled02 == true) {
-        // On définit une position pour le control camera
-        // var offset = new THREE.Vector3(5, 0, 7)
-        // controlPosition = position de l'atome + décalage de 7 unité sur l'axe z
-        var controlPosition = electronMesh2.position.clone()
-            // Applique la nouvelle position
+    // Si le focus de l'atome est activé
+    if (_focusLune == Lune) {
+
+        // Offset de la caméra par rapport à la planète (offset = rayon planète + 10)
+        var offset = new THREE.Vector3(0, 0, 25)
+            // Déclare un Vector3 worldPosition
+        var worldPosition = new THREE.Vector3()
+            // Enregistre la world position de la Terre (position globale) dans worldPosition
+        Lune.getWorldPosition(worldPosition)
+            // Applique la position au control
+        var controlPosition = worldPosition.add(offset)
         _control.object.position.copy(controlPosition)
-            //  focus le control sur la position de l'atome
-        _control.target = electronMesh2.position
+            // Focus la world position de la Terre
+        _control.target = worldPosition
     }
+
+    // Si le focus de l'atome est activé
+    if (_focusMars == true) {
+
+        // Offset de la caméra par rapport à la planète (offset = rayon planète + 10)
+        var offset = new THREE.Vector3(0, 0, 25)
+            // Déclare un Vector3 worldPosition
+        var worldPosition = new THREE.Vector3()
+            // Enregistre la world position de la Terre (position globale) dans worldPosition
+        Mars.getWorldPosition(worldPosition)
+            // Applique la position au control
+        var controlPosition = worldPosition.add(offset)
+        _control.object.position.copy(controlPosition)
+            // Focus la world position de la Terre
+        _control.target = worldPosition
+    }
+
+    // Si le focus de l'atome est activé
+    if (_focusSaturn == true) {
+
+        // Offset de la caméra par rapport à la planète (offset = rayon planète + 10)
+        var offset = new THREE.Vector3(0, 30, 60)
+            // Déclare un Vector3 worldPosition
+        var worldPosition = new THREE.Vector3()
+            // Enregistre la world position de la Terre (position globale) dans worldPosition
+        Saturne.getWorldPosition(worldPosition)
+            // Applique la position au control
+        var controlPosition = worldPosition.add(offset)
+        _control.object.position.copy(controlPosition)
+            // Focus la world position de la Terre
+        _control.target = worldPosition
+    }
+    // Si le focus de l'atome est activé
+    if (_focusSoleil == true) {
+
+        // Offset de la caméra par rapport à la planète (offset = rayon planète + 10)
+        var offset = new THREE.Vector3(0, 0, 25)
+            // Déclare un Vector3 worldPosition
+        var worldPosition = new THREE.Vector3()
+            // Enregistre la world position de la Terre (position globale) dans worldPosition
+        Soleil.getWorldPosition(worldPosition)
+            // Applique la position au control
+        var controlPosition = worldPosition.add(offset)
+        _control.object.position.copy(controlPosition)
+            // Focus la world position de la Terre
+        _control.target = worldPosition
+    }
+
+
 
     // Mise à jour du control  ( /!\ necessaire car on utilise le damping)
     _control.update()
